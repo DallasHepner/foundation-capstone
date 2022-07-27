6b4f9332971d9a0427fe3539c60d86d191e9eb47
Dallas Hepner <dallashepner@gmail.com>
2022-07-27 15:23:56 -0600

    DB Seed working

--- server/index.js
+++ server/index.js
@@ -3,14 +3,13 @@ const express = require('express')
 const path = require('path')
 const cors = require('cors')
 const app = express()
+const {SERVER_PORT} = process.env
+const {seed} = require('./seed.js')
+// const {} = require('./controller.js')
 
 app.use(express.json())
 app.use(cors())
 
+app.post('/seed', seed)
 
-
-const port = process.env.PORT || 4044
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
